8a6b7a427cea62f75788b86fbbb356ac4b209f69
Theo S <iosdeveloper@tgs.me.uk>
2025-12-22 11:41:16 +0000

    Add Xcode Build Macros

---
+++ src/configall_system_aaplxcode.h
@@ -0,0 +1,119 @@
+/* This is the header file for the Apple (Xcode Clang) compiler, defining all
+ * system and compiler dependent configuration macros */
+
+/* Define to 1, 2, 3, or 4 if Aboca should be build. */
+/* #undef CLP_HAS_ABC */
+
+/* Define to the debug sanity check level (0 is no test) */
+#define COIN_CLP_CHECKLEVEL 0
+
+/* Define to the debug verbosity level (0 is no output) */
+#define COIN_CLP_VERBOSITY 0
+
+/* Define to 1 if the AMD package is available */
+/* #undef COIN_HAS_AMD */
+
+/* Define to 1 if the ASL package is available */
+/* #undef COIN_HAS_ASL */
+
+/* If defined, the BLAS Library is available. */
+/* #undef COIN_HAS_BLAS */
+
+/* Define to 1 if the CHOLMOD package is available */
+/* #undef COIN_HAS_CHOLMOD */
+
+/* Define to 1 if the CoinUtils package is available */
+#define COIN_HAS_COINUTILS 1
+
+/* Define to 1 if the Glpk package is available */
+/* #undef COIN_HAS_GLPK */
+
+/* Define to 1 if the Mumps package is available */
+/* #undef COIN_HAS_MUMPS */
+
+/* Define to 1 if the Netlib package is available */
+#define COIN_HAS_NETLIB 1
+
+/* Define to 1 if the Osi package is available */
+#define COIN_HAS_OSI 1
+
+/* Define to 1 if the OsiTests package is available */
+#define COIN_HAS_OSITESTS 1
+
+/* Define to 1 if readline is available */
+/* #undef COIN_HAS_READLINE */
+
+/* Define to 1 if the Sample package is available */
+#define COIN_HAS_SAMPLE 1
+
+/* Define to 1 if the WSMP package is available */
+/* #undef COIN_HAS_WSMP */
+
+/* Define to dummy `main' function (if any) required to link to the Fortran
+ libraries. */
+/* #undef F77_DUMMY_MAIN */
+
+/* Define to a macro mangling the given C identifier (in lower and upper
+ case), which must not contain underscores, for linking with Fortran. */
+/* #undef F77_FUNC */
+
+/* As F77_FUNC, but for C identifiers containing underscores. */
+/* #undef F77_FUNC_ */
+
+/* Define if F77 and FC dummy `main' functions are identical. */
+/* #undef FC_DUMMY_MAIN_EQ_F77 */
+
+/* Define to 1 if you have the <cfloat> header file. */
+#define HAVE_CFLOAT 1
+
+/* Define to 1 if you have the <cieeefp> header file. */
+/* #undef HAVE_CIEEEFP */
+
+/* Define to 1 if you have the <cmath> header file. */
+#define HAVE_CMATH 1
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the <float.h> header file. */
+/* #undef HAVE_FLOAT_H */
+
+/* Define to 1 if you have the <ieeefp.h> header file. */
+/* #undef HAVE_IEEEFP_H */
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <math.h> header file. */
+/* #undef HAVE_MATH_H */
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <readline/readline.h> header file. */
+/* #undef HAVE_READLINE_READLINE_H */
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+
+
+
