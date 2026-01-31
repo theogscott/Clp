1347b44377b3734a32377903466b767cf91ee08c
Theo S <iosdeveloper@tgs.me.uk>
2026-01-31 18:34:10 +0000

    adding an Xcode project for the example code targets

---
+++ Xcode/Clp/ClpExamplesMinimum/mainTest.cpp
@@ -0,0 +1,21 @@
+//
+//  mainTest.cpp
+//  ClpExamplesMinimum
+//
+//  Created by Theo S   on 30/01/2026.
+//
+
+
+//  a dummy for now, until
+//
+
+#include <stdio.h>
+
+#include "ClpSimplex.hpp"
+
+int mainTest(int argc, const char *argv[], int algorithm,
+             ClpSimplex empty, ClpSolve solveOptionsIn,
+  int switchOffValue, bool doVector)
+{
+    return 0;
+}
